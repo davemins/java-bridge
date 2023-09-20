c8d10c71626041e01de859c2a652335e1bf56336
davemins <davemins4125@naver.com>
2023-09-20 15:46:44 +0900

    chore : 주석 추가 - 문제 요약

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,3 +1,17 @@
+/*
+위아래 둘 중 하나의 칸만 건널 수 있는 다리를 끝까지 건너가는 게임이다.
+위 칸을 건널 수 있는 경우 U, 아래 칸을 건널 수 있는 경우 D값으로 나타낸다.
+무작위 값이 0인 경우 아래 칸, 1인 경우 위 칸이 건널 수 있는 칸이 된다.
+이동한 칸을 건널 수 있다면 O로 표시한다. 건널 수 없다면 X로 표시한다.
+
+함수(또는 메서드)의 길이가 10라인을 넘어가지 않도록 구현
+메서드의 파라미터 개수는 최대 3개까지만 허용
+
+클래스별 세부 설명 참고
+BridgeGame, BridgeMaker, BridgeNumberGenerator,
+BridgeRandomNumberGenerator, InputView, OutputView
+ */
+
 package bridge;
 
 public class Application {
